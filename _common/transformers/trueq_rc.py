28f2345c2fd5a41cf8a02619e5ff63ac3afc020d
hammamqassim <hammam.qassim@keysight.com>
2022-07-06 11:51:32 -0400

    Removed temporary work-around in Qiskit/True-Q conversion. The workaround is not needed in latest version of True-Q

--- _common/transformers/trueq_rc.py
+++ _common/transformers/trueq_rc.py
@@ -22,14 +22,10 @@ def full_rc(circuit, backend, n_compilations=20):
     tq.interface.qiskit.set_from_backend(backend)
     circuit_tq, metadata = tq.interface.qiskit.to_trueq_circ(circuit)
     
-    for qubit in circuit.qubits:
-        if qubit.index not in circuit_tq.labels:
-            del metadata.mapping[qubit.index]
-    
     rc_collection = [
                 tq.interface.qiskit.from_trueq_circ(compiler.compile(circuit_tq), 
-                metadata=metadata)
-                for j in range(n_compilations)
+                                                    metadata=metadata) 
+        for j in range(n_compilations)
     ]
     return rc_collection
 
@@ -42,22 +38,18 @@ def local_rc(circuit, backend, n_compilations=20):
             tqc.UnmarkCycles,
             tqc.MarkCycles,
             tqc.RCLocal,
-            tqc.UnmarkCycles,
+            #tqc.UnmarkCycles,
             tqc.Justify
         )
     )
 
     tq.interface.qiskit.set_from_backend(backend)
     circuit_tq, metadata = tq.interface.qiskit.to_trueq_circ(circuit)
-    
-    for qubit in circuit.qubits:
-        if qubit.index not in circuit_tq.labels:
-            del metadata.mapping[qubit.index]
 
     rc_collection = [
                 tq.interface.qiskit.from_trueq_circ(compiler.compile(circuit_tq), 
-                metadata=metadata)
-                for j in range(n_compilations)
+                                                    metadata=metadata) 
+        for j in range(n_compilations)
     ]
     
     return rc_collection
